62b0022c05a59c8fc9eab86e84254aa5e34a8d2b
John <john.19col@gmail.com>
2020-01-09 05:04:41 -0500

    Configure apollo client + connect it to react app

--- client/index.js
+++ client/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+const cache = new InMemoryCache({
+  dataIdFromObject: object => object.id
+});
+
+const link = new HttpLink({
+  uri: '/graphql',
+  credentials: 'same-origin'
+});
+
+const client = new ApolloClient({ cache, link });
+
 const Root = () => {
-  return <div>Auth Starter</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>Auth Starter</div>;
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(<Root />, document.querySelector('#root'));
