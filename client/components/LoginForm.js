6599ffed67a9158cf26f051ad24db12ff71523e3
John <john.19col@gmail.com>
2020-01-21 14:19:36 -0500

    Refresh current user query after login mutation

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useMutation } from '@apollo/react-hooks';
 
-import { Mutation } from './../graphql';
+import { Mutation, Query } from './../graphql';
 import { AuthForm } from './AuthForm';
 
 export const LoginForm = () => {
-  const [loginMutation] = useMutation(Mutation.login, {});
+  const [loginMutation] = useMutation(Mutation.login);
   const login = credentials => {
-    loginMutation({ variables: credentials })
+    loginMutation({
+      variables: credentials,
+      refetchQueries: [{ query: Query.currentUser }]
+    })
       .then(({ data }) => console.log(`${data.login.name} logged successfully`))
       .catch(error => console.log(error.message));
   };
