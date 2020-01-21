ed45d1b0dc8e35e82a174d7308d560e9cd85de9f
John <john.19col@gmail.com>
2020-01-21 13:59:45 -0500

    Use login mutation in login form

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useMutation } from '@apollo/react-hooks';
 
+import { Mutation } from './../graphql';
 import { AuthForm } from './AuthForm';
 
 export const LoginForm = () => {
-  const login = credentials => console.log('credentials:', credentials);
+  const [loginMutation] = useMutation(Mutation.login, {});
+  const login = credentials => {
+    loginMutation({ variables: credentials })
+      .then(({ data }) => console.log(`${data.login.name} logged successfully`))
+      .catch(error => console.log(error.message));
+  };
   return <AuthForm title="Login" onSubmit={login}></AuthForm>;
 };
