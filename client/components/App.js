931125912f4887e91f3b4ff0bb8bce47bbcf69f3
John <john.19col@gmail.com>
2020-01-23 10:09:56 -0500

    Create Header component

--- client/components/App.js
+++ client/components/App.js
@@ -1,45 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useQuery, useMutation } from '@apollo/react-hooks';
 
-import { Query, Mutation } from './../graphql';
+import { Header } from './Header';
 
 export const App = ({ children }) => {
-  const { data, loading } = useQuery(Query.currentUser);
-
-  const [logout] = useMutation(Mutation.logout, {
-    refetchQueries: [{ query: Query.currentUser }]
-  });
-
-  if (loading) {
-    return null;
-  }
-
-  const { currentUser } = data;
-  const links = getLinks(currentUser, logout);
-
   return (
     <div className="container">
-      <header>
-        <nav>
-          <ul>
-            {links.map((link, i) => (
-              <li key={i}>{link}</li>
-            ))}
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <React.Fragment>{children}</React.Fragment>
-      </main>
+      <Header />
+      <main>{children}</main>
     </div>
   );
-};
-
-const getLinks = (currentUser, logout) => {
-  if (currentUser) {
-    return [<a onClick={logout}>Logout</a>];
-  }
-
-  return [<Link to="/login">Login</Link>, <Link to="/signup">Signup</Link>];
 };
---
+++ client/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+
+import { Query, Mutation } from './../graphql';
+
+export const Header = () => {
+  const { data, loading } = useQuery(Query.currentUser);
+
+  const [logout] = useMutation(Mutation.logout, {
+    refetchQueries: [{ query: Query.currentUser }]
+  });
+
+  if (loading) {
+    return null;
+  }
+
+  const { currentUser } = data;
+  const links = getLinks(currentUser, logout);
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          {links.map((link, i) => (
+            <li key={i}>{link}</li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+const getLinks = (currentUser, logout) => {
+  if (currentUser) {
+    return [<a onClick={logout}>Logout</a>];
+  }
+
+  return [<Link to="/login">Login</Link>, <Link to="/signup">Signup</Link>];
+};
